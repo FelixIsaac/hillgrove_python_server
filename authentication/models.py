bc6b81000fa8e3192a4c1df66567abd8de81c812
Felix Isaac <limfelix16@gmail.com>
2021-06-29 02:05:13 +0800

    feat(users): add encryption

--- authentication/models.py
+++ authentication/models.py
@@ -1,25 +1,27 @@
 from django.db import models
-
-# Create your models here.
+from mirage import fields
 
 
 class User(models.Model):
     google_id = models.CharField(max_length=21, unique=True)
     avatar = models.CharField(max_length=43)
-    email = models.EmailField(max_length=64, unique=True)
+    email = fields.EncryptedEmailField(max_length=64, unique=True)
     name = models.CharField(max_length=254)
     first_name = models.CharField(max_length=254)
     family_name = models.CharField(max_length=254)
     created_on = models.DateTimeField(auto_now_add=True)
     xp = models.PositiveIntegerField(default=100)
 
-    def avatar_urL(self):
+    @property
+    def avatar_url(self):
         return 'https://lh3.googleusercontent.com/a/{}=s96-'.format(self.avatar)
 
+    @property
     def add_xp(self, xp, *args, **kwargs):
         self.xp += int(xp)
         super(User, self).save(*args, **kwargs)
 
+    @property
     def update_avatar(self, avatar, *args, **kwargs):
         self.avatar = avatar
         super(User, self).save(*args, **kwargs)
