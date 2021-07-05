06c5337526e25dc4b404803b24577e5e46b7fc8f
Felix Isaac <limfelix16@gmail.com>
2021-07-06 00:52:59 +0800

    feat(session): add session and topic models

---
+++ session/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+import re
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    session = models.ForeignKey('session.Session', on_delete=models.CASCADE)
+
+    @property
+    def url(self):
+        return re.sub(r'[\W_]+', '-', self.name).lower()
+
+# Create your models here.
+
+
+class Session(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.CharField(max_length=254, unique=True)
+    topics = models.ManyToManyField('session.Topic', related_name='session_topics')
+    enabled = models.BooleanField(default=False)
+
+    @property
+    def url(self):
+        return re.sub(r'[\W_]+', '-', self.name).lower()
