c2f54d6d6849e5d57c5e2ea44c26549d15ce5583
Felix Isaac <limfelix16@gmail.com>
2021-07-07 00:40:28 +0800

    Update urls.py

--- session/urls.py
+++ session/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_sessions, name="get-sessions"),
+    path('all', views.get_sessions, name="get-sessions"),
     path('<session>', views.get_sessions, name="get-sessions")
 ]
