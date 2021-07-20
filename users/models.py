8d87899982d9ec727477c73adb59d7f86e6cd385
Felix Isaac <limfelix16@gmail.com>
2021-07-20 22:39:22 +0800

    feat(solution progress): add solution progress table

--- users/models.py
+++ users/models.py
@@ -3,6 +3,8 @@ from authentication.models import User
 import session.models as session
 
 # Create your models here.
+
+
 class Progress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     last_session = models.ForeignKey(session.Session, on_delete=models.CASCADE)
@@ -11,9 +13,22 @@ class Progress(models.Model):
     class Meta:
         unique_together = ('user', 'last_session')
 
-class SeenSolutions(models.Model):
-    user = models.ForeginKey(User, on_delete=models.CASCADE)
-    solution = models.ForeginKey(session.Solution, on_delete=models.CASCADE)
+
+# class SeenSolutions(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     solution = models.ForeignKey(session.Solution, on_delete=models.CASCADE)
+
+#     class Meta:
+#         unique_together = ('user', 'solution')
+
+class SolutionProgress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    solution = models.ForeignKey(session.Solution, on_delete=models.CASCADE)
+    attempts = models.IntegerField(default=0)
+    shown_hint = models.BooleanField(default=False)
+    shown_solution = models.BooleanField(default=False)
+    solution_code = models.TextField()
+    draft_code = models.TextField()
 
     class Meta:
         unique_together = ('user', 'solution')
